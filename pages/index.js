146cb33d1a707cdfb13474272edd759ac40e088e
nipunagarwal4235 <nipunagarwal2002@gmail.com>
2022-07-16 05:14:55 +0530

    fixing deployment

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
 import NavBar from "../Components/NavBar.jsx/NavBar";
 import AmongUs from "../Components/AmongUs";
-import Games from "../Components/Games";
+// import Games from "../Components/Games";
 
 export default function Home() {
   return (
     <>
       <NavBar></NavBar>
       <AmongUs />
-      <Games />
+      {/* <Games /> */}
     </>
   );
 }
